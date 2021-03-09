f202b3cd8100e48f0c6794dd093742ab5caf23ee
Melxi <magomed.futijev@gmail.com>
2021-03-09 22:57:05 +0200

    8. Selenium, eli web-selaimen simulointi ohjelmakoodista

--- viikko3/tehtavat4-11/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/tehtavat4-11/WebLogin/src/main/java/ohtu/Tester.java
@@ -12,22 +12,91 @@ public class Tester {
 
         driver.get("http://localhost:4567");
         
+        // sleep(2);
+        
+        // WebElement element = driver.findElement(By.linkText("login"));
+        // element.click();
+
+        // sleep(2);
+
+        // element = driver.findElement(By.name("username"));
+        // element.sendKeys("pekka");
+        // element = driver.findElement(By.name("password"));
+        // element.sendKeys("akkep");
+        // element = driver.findElement(By.name("login"));
+        
+        // sleep(2);
+        // element.submit();
+
+        // sleep(3);
+
+        // Failed logging in: correct user, wrong password
+        // sleep(2);
+        
+        // WebElement element = driver.findElement(By.linkText("login"));
+        // element.click();
+
+        // sleep(2);
+
+        // element = driver.findElement(By.name("username"));
+        // element.sendKeys("pekka");
+        // element = driver.findElement(By.name("password"));
+        // element.sendKeys("wrong");
+        // element = driver.findElement(By.name("login"));
+        
+        // sleep(2);
+        // element.submit();
+
+        // sleep(3);
+
+        // Creating a new user
+        // sleep(2);
+        
+        // WebElement element = driver.findElement(By.linkText("register new user"));
+        // element.click();
+
+        // sleep(2);
+
+        // element = driver.findElement(By.name("username"));
+        // element.sendKeys("new");
+        // element = driver.findElement(By.name("password"));
+        // element.sendKeys("test");
+        // element = driver.findElement(By.name("passwordConfirmation"));
+        // element.sendKeys("test");
+        // element = driver.findElement(By.name("signup"));
+        
+        // sleep(2);
+        // element.submit();
+
+        // sleep(3);
+
+        // Logging out after successfully creating a user
         sleep(2);
         
-        WebElement element = driver.findElement(By.linkText("login"));
+        WebElement element = driver.findElement(By.linkText("register new user"));
         element.click();
 
         sleep(2);
 
         element = driver.findElement(By.name("username"));
-        element.sendKeys("pekka");
+        element.sendKeys("another");
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
-        element = driver.findElement(By.name("login"));
+        element.sendKeys("test");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("test");
+        element = driver.findElement(By.name("signup"));
         
         sleep(2);
         element.submit();
 
+        sleep(3);
+
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        sleep(2);
+
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
         sleep(3);
         
         driver.quit();
